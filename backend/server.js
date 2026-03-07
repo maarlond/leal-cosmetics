af69839848c3a39acc8dfdba74ad2ec1ea868378
maarlond <48226010+maarlond@users.noreply.github.com>
2026-03-06 21:02:16 -0300

    Ajuste porta servidor

--- backend/server.js
+++ backend/server.js
@@ -152,6 +152,12 @@ app.use("/", produtosRouter);    // /produtos, /produtos/:id etc.
 // Todas as rotas de produtos
 //app.use("/", produtosRouter);
 
-app.listen(3000, () => {
+/*app.listen(3000, () => {
     console.log("Servidor rodando em http://localhost:3000");
+});*/
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando na porta", PORT);
 });
